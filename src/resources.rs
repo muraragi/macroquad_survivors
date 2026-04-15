17f56873e4f8a8de0c1abfeec877599e961628ad
m.yzbashev <m.yzbashev@rt-solar.ru>
2026-04-15 04:50:04 +0300

    added enemy spawner

--- src/resources.rs
+++ src/resources.rs
@@ -8,3 +8,29 @@ pub struct ScreenSize {
     pub width: f32,
     pub height: f32,
 }
+
+#[derive(Resource)]
+pub struct Timer {
+    elapsed: f32,
+    interval: f32,
+}
+
+impl Timer {
+    pub fn new(interval: f32) -> Self {
+        Self {
+            interval,
+            elapsed: 0.0,
+        }
+    }
+
+    pub fn tick(&mut self, delta: f32) -> bool {
+        self.elapsed += delta;
+
+        if self.elapsed >= self.interval {
+            self.elapsed -= self.interval;
+            return true;
+        }
+
+        false
+    }
+}
